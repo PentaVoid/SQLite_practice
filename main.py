37a9bcc3fc8e7abb71ce19471b73419716b39867
Ayo Soile <voidpenta@gmail.com>
2022-10-13 13:52:55 -0400

    sql lite

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import sqlite3
+
+connection = sqlite3.connect("gta.db")
+cursor = connection.cursor()
+
+cursor.execute("create table gta (release_year integer, release_name text, city text)")
 release_list = [
     (1997, "Grand Theft Auto", "state of New Guernsey"),
     (1999, "Grand Theft Auto 2", "Anywhere, USA"),
@@ -7,3 +13,27 @@ release_list = [
     (2008, "Grand Theft Auto IV", "Liberty City"),
     (2013, "Grand Theft Auto V", "Los Santos")
 ]    
+
+cursor.executemany("insert into gta values (?,?,?)", release_list)
+
+for row in cursor.execute("select * from gta"):
+    print(row)
+
+print("*************************************")
+cursor.execute("Select * from gta where city=:c", {"c": "Liberty City"})
+gta_search = cursor.fetchall()
+print(gta_search)
+
+
+cursor.execute("create table cities (gta_city text, real_city text)")
+cursor.execute("insert into cities values (?,?)", ("Liberty City", "New York"))
+cursor.execute("Select * from cities where gta_city=:c", {"c": "Liberty City"})
+cities_search = cursor.fetchall()
+print(cities_search)
+connection.close()
+
+print("******************************")
+
+for i in gta_search:
+    adjusted = [cities_search[0][1] if value== cities_search[0][0] else value for value in i]
+    print(adjusted)
